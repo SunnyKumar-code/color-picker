9867fc8c99ff3d3dc4205b99ed36e4499cbbd573
Sunny Kumar <sk202542@gmail.com>
2024-12-17 23:42:05 +0530

    copy code

--- src/componant/card.jsx
+++ src/componant/card.jsx
@@ -1,12 +1,15 @@
 import styles from './card.module.css'
 const Card = (props)=>{
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(props.hex);
+        alert(`Copied: ${props.hex}`); 
+      };
     return(
-<div className={styles.Card}>
+<div className={styles.Card}  onClick={copyToClipboard}>
     <div className={styles.colorBox} style={{backgroundColor:props.hex}}></div>
     <div className={styles.text}>
         <p style={{fontSize:"18px"}}>{props.hex}</p>
         <p style={{ color: props.hex, fontSize: "12px" }}>{props.color}</p>
-
     </div>
 </div>
     )
